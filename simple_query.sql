5ec066d7d7cedd518d062a566ced56964dd92a34
Sasha Chernyshova <alexandrachernyshova07@gmail.com>
2020-12-13 21:59:43 +0300

    SIMPLE-QUERY add simple queries

---
+++ simple_query.sql
@@ -0,0 +1,10 @@
+-- SIMPLE QUERIES --
+
+SELECT title, author FROM exhibit WHERE style = ‘Postimpressionism’;
+/* Shows titles and authors in Post-impressionism style */
+
+SELECT title, salary FROM position WHERE salary > 5000 ORDER BY salary DESC;
+/* Shows position and it's salary if salary is over 5000 sorted by salary */
+
+SELECT * FROM exhibit WHERE type = ‘Sculpture’;
+/* Shows info about all sculptures in the museum */
